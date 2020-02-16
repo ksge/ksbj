22632f55d11d441f5e429f25e2896eb2ffd1fda4
ksge <44881679+ksge@users.noreply.github.com>
2020-02-16 12:28:23 +0000

    Update ksbj-start-win.bat

--- ksbj-start-win.bat
+++ ksbj-start-win.bat
@@ -1,6 +1,7 @@
 
-rem bj.exe row number   game name   model name   debug(0=no 1=yes)       &
-ksbj.exe 2 "KISS STRIP BLACKJACK" "ANNA" 0 &
-rem ksge.exe model name debug(0=no 1=yes)  clip file format
-ksge.exe "ANNA" 0 "mkv"
+rem ksbj.exe row number   game name   model name   debug(0=no 1=yes)
+rem for bj use always & on after final character
 
+start ksbj.exe 2 "KISS STRIP BLACKJACK" "JENNIFER" 0 
+rem ksge.exe model name debug(0=no 1=yes)  clip file format
+start ksge.exe "JENNIFER" 0 "mkv"
